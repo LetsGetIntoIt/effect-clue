2163785c0f6c5cc578219dd570a826a2efa7dab6
Kapil Easwar <1480995+bigpopakap@users.noreply.github.com>
2023-06-26 11:53:42 -0700

    feat: start putting hardcoded data into the UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,10 @@
 import { render } from 'preact';
 import './style.css';
 
+import { Clue } from './ui/Clue';
+
 export function App() {
-	return (
-		<>
-			<aside class="game-setup">
-				<section class="panel players">
-					<h2>Players</h2>
-				</section>
-
-				<section class="panel cards">
-					<h2>Cards</h2>
-				</section>
-			</aside>
-
-			<main class="panel checklist">
-				<h2>Checklist</h2>
-			</main>
-
-			<aside class="panel guesses">
-				<h2>Guesses</h2>
-			</aside>
-		</>
-	);
+	return Clue();
 }
 
 let container;
---
+++ src/ui/Clue.tsx
@@ -0,0 +1,77 @@
+import { signal } from "@preact/signals";
+
+import './Clue.css';
+
+export function Clue() {
+	const standardCards = signal<'North America' | undefined>('North America');
+
+	const cards = signal<[string, string][]>([
+		['Room', 'Dog house'],
+	]);
+
+	const players = signal<[string][]>([
+		['Kapil'],
+		['Kate'],
+	]);
+
+	return (
+		<>
+			<aside class="game-setup">
+				<section class="panel players">
+					<h2>Players</h2>
+					
+					{players.value.map(player => (
+						<input type="text" value={player[0]} disabled />
+					))}
+				</section>
+
+				<section class="panel cards">
+					<h2>Cards</h2>
+
+					<input id="standardCards" type="checkbox" checked={standardCards.value === 'North America'} label="North America" disabled />
+					<label for="#standardCards">North America set</label>
+
+					{cards.value.map(card => (
+						<input type="text" value={`${card[0]}: ${card[1]}`} disabled />
+					))}
+				</section>
+			</aside>
+
+			<main class="panel checklist">
+				<h2>Checklist</h2>
+
+				<table>
+					<thead>
+						<tr>
+							{/* Blank heading for the cards row */}
+							<th/>
+
+							{/* Player name column headings */}
+							{players.value.map(([playerName]) => <th>
+								<h3>{playerName}</h3>
+								<label>_ cards</label>
+							</th>)}
+						</tr>
+					</thead>
+
+					{cards.value.map(([cardType, cardName]) => (
+						<tr>
+							<td>
+								<h3>{cardName}</h3>
+								<label>{cardType}</label>
+							</td>
+
+							{players.value.map(([playerName]) => (<td>
+								'yes/no'
+							</td>))}
+						</tr>
+					))}
+				</table>
+			</main>
+
+			<aside class="panel guesses">
+				<h2>Guesses</h2>
+			</aside>
+		</>
+	);
+}
