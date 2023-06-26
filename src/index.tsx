e68a6417ab88b4bd5f84f789e040fd96fcd0de2e
Kapil Easwar <1480995+bigpopakap@users.noreply.github.com>
2023-06-26 11:23:00 -0700

    feat: initial page layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,26 @@
 import { render } from 'preact';
-import { useState } from 'preact/hooks';
-import preactLogo from './assets/preact.svg';
-import viteLogo from '/vite.svg';
 import './style.css';
 
 export function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} class="logo" alt="Vite logo" />
-				</a>
-				<a href="https://preactjs.com" target="_blank">
-					<img src={preactLogo} class="logo preact" alt="Preact logo" />
-				</a>
-			</div>
-			<h1>Vite + Preact</h1>
-			<div class="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/app.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p class="read-the-docs">Click on the Vite and Preact logos to learn more</p>
+			<aside class="game-setup">
+				<section class="panel players">
+					<h2>Players</h2>
+				</section>
+
+				<section class="panel cards">
+					<h2>Cards</h2>
+				</section>
+			</aside>
+
+			<main class="panel checklist">
+				<h2>Checklist</h2>
+			</main>
+
+			<aside class="panel guesses">
+				<h2>Guesses</h2>
+			</aside>
 		</>
 	);
 }
